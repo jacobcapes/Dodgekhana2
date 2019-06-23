8f7852f3d498876bf4e61fba94e2bd0b59477a66
Jacob Capes <jacobcapes@mmc.school.nz>
2019-06-24 09:44:40 +1200

    Drawn rectangles on panel game

--- Dodgekhana2/Form1.Designer.cs
+++ Dodgekhana2/Form1.Designer.cs
@@ -28,13 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.PnlGame = new System.Windows.Forms.Panel();
+            this.TmrTire = new System.Windows.Forms.Timer(this.components);
+            this.TmrVehicle = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // PnlGame
+            // 
+            this.PnlGame.BackColor = System.Drawing.Color.Gray;
+            this.PnlGame.Location = new System.Drawing.Point(2, 41);
+            this.PnlGame.Name = "PnlGame";
+            this.PnlGame.Size = new System.Drawing.Size(375, 325);
+            this.PnlGame.TabIndex = 0;
+            this.PnlGame.Paint += new System.Windows.Forms.PaintEventHandler(this.PnlGame_Paint);
+            // 
+            // TmrTire
+            // 
+            this.TmrTire.Tick += new System.EventHandler(this.TmrTire_Tick);
             // 
             // FrmDodge
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(436, 368);
+            this.Controls.Add(this.PnlGame);
             this.Name = "FrmDodge";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +61,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel PnlGame;
+        private System.Windows.Forms.Timer TmrTire;
+        private System.Windows.Forms.Timer TmrVehicle;
     }
 }
 
--- Dodgekhana2/Form1.cs
+++ Dodgekhana2/Form1.cs
@@ -19,17 +19,52 @@ namespace Dodgekhana2
         // Declare the rectangles to display the spaceship and planets in
         Rectangle area, area1, area2, area3, area4, area5, area6, area7;
         int x2 = 50, y2 = 290; // starting position of spaceship
+
+        private void PnlGame_Paint(object sender, PaintEventArgs e)
+        {
+            //get the methods from the graphic's class to paint on the panel
+            g = e.Graphics;
+            //use the DrawImage method to draw the spaceship on the panel
+            g.DrawImage(Vehicle, area);
+            //use the DrawImage method to draw the planet on the panel
+            g.DrawImage(Tire, area1);
+            g.DrawImage(Tire, area2);
+            g.DrawImage(Tire, area3);
+            g.DrawImage(Tire, area4);
+            g.DrawImage(Tire, area5);
+            g.DrawImage(Tire, area6);
+            g.DrawImage(Tire, area7);
+        }
+
+        private void TmrTire_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
         int x3 = 50, y5 = 290; // starting position of spaceship
         int score = 0;
         int lives = 5;
 
         // load the spaceship and one planet
-        Image spaceship = Image.FromFile(Application.StartupPath + @"\Vehicle.png");
-        Image planet1 = Image.FromFile(Application.StartupPath + @"\Tire.png");
+        Image Vehicle = Image.FromFile(Application.StartupPath + @"\Vehicle.jpg");
+        Image Tire = Image.FromFile(Application.StartupPath + @"\Tire.png");
         bool left, right, up, down;
         public FrmDodge()
         {
             InitializeComponent();
+            area = new Rectangle(x2, y2, 30, 30);//spaceship's rectangle
+            area1 = new Rectangle(x, y, 20, 20);// planet1's rectangle
+            area2 = new Rectangle(x + 50, y, 20, 20);// planet2's rectangle
+            area3 = new Rectangle(x + 100, y, 20, 20);// planet3's rectangle
+            area4 = new Rectangle(x + 150, y, 20, 20);// planet4's rectangle
+            area5 = new Rectangle(x + 200, y, 20, 20);// planet5's rectangle
+            area6 = new Rectangle(x + 250, y, 20, 20);// planet6's rectangle
+            area7 = new Rectangle(x + 300, y, 20, 20);// planet7's rectangle
         }
 
         private void Form1_Load(object sender, EventArgs e)
