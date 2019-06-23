e161e39d7ded18ef5826e9e13fecf76c16e8769d
Jacob Capes <jacobcapes@mmc.school.nz>
2019-06-24 09:24:06 +1200

    Added Variables

--- Dodgekhana2/Form1.cs
+++ Dodgekhana2/Form1.cs
@@ -10,11 +10,25 @@ using System.Windows.Forms;
 
 namespace Dodgekhana2
 {
-    public partial class Form1 : Form
+    Graphics g; // declare the graphics object
+    int x = 20, y = 20;// starting position of tire
+    Random speed = new Random();
+    // Declare the rectangles to display the spaceship and planets in
+    Rectangle area, area1, area2, area3, area4, area5, area6, area7;
+    int x2 = 50, y2 = 290; // starting position of spaceship
+    int x3 = 50, y5 = 290; // starting position of spaceship
+    int score = 0;
+    int lives = 5;
+    public partial class FrmDodge : Form
     {
-        public Form1()
+        public FrmDodge()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
