ee0e96fa487ee8668208f784f26411d7ff43590d
Jacob Capes <jacobcapes@mmc.school.nz>
2019-06-24 09:29:10 +1200

    Added proper variables in correct place

--- Dodgekhana2/Form1.cs
+++ Dodgekhana2/Form1.cs
@@ -10,17 +10,23 @@ using System.Windows.Forms;
 
 namespace Dodgekhana2
 {
-    Graphics g; // declare the graphics object
-    int x = 20, y = 20;// starting position of tire
-    Random speed = new Random();
-    // Declare the rectangles to display the spaceship and planets in
-    Rectangle area, area1, area2, area3, area4, area5, area6, area7;
-    int x2 = 50, y2 = 290; // starting position of spaceship
-    int x3 = 50, y5 = 290; // starting position of spaceship
-    int score = 0;
-    int lives = 5;
+   
     public partial class FrmDodge : Form
     {
+        Graphics g; // declare the graphics object
+        int x = 20, y = 20;// starting position of tire
+        Random speed = new Random();
+        // Declare the rectangles to display the spaceship and planets in
+        Rectangle area, area1, area2, area3, area4, area5, area6, area7;
+        int x2 = 50, y2 = 290; // starting position of spaceship
+        int x3 = 50, y5 = 290; // starting position of spaceship
+        int score = 0;
+        int lives = 5;
+
+        // load the spaceship and one planet
+        Image spaceship = Image.FromFile(Application.StartupPath + @"\Vehicle.png");
+        Image planet1 = Image.FromFile(Application.StartupPath + @"\Tire.png");
+        bool left, right, up, down;
         public FrmDodge()
         {
             InitializeComponent();
